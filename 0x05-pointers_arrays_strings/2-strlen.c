77b2a9c1fa995556736f86963817c0a97a1c3f08
yunak24 <yunakoyee@gmail.com>
2023-03-27 16:39:53 -0700

    modified strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns length as integer;
- */
+ * _strlen - function that returns the length
+ * of a string
+ * @s: variable tobe returned
+ * Return: always 0
+*/
 
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(s + len) != '\0')
+	while (*s != '\0')
+	{
 		len++;
-
+		s++;
+	}
 	return (len);
 }
