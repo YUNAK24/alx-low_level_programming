46ed206a60e1f831d5f24b9021635d48223c3152
yunak24 <yunakoyee@gmail.com>
2023-03-27 15:20:30 -0700

    main

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
 *reset_to_98 - resets the value of given integer to 98
@@ -8,5 +8,5 @@
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
 * void prototypes()
 * int prototypes()
 */
 
-void reset_to_98(int *n);
+void reset_to_98(int *a);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -14,8 +14,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
+void print_array(int *a,  int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
-endif // MAIN_H_INCLUDED
+endif /*MAIN_H*/
