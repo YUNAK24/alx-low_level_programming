49cbac986ae156826f042791c55cd6da12869d82
yunak24 <yunakoyee@gmail.com>
2023-03-28 15:03:11 -0700

    print_rev

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,21 +2,21 @@
 #include "main.h"
 
 /**
- *_puts - prints a string
- *@str: string to print
- *
- *Description: print a string
- *On success: returns no error
- */
+*_puts - prints a string
+*@str: string to print
+*
+*Description: print a string
+*On success: returns no error
+*/
 
 void _puts(char *str)
 {
-	int i = 0;
-	
-	while (*(str + i) != '\0')
-	{
-		putchar(*(str + i));
-		i++;
-	}
-	putchar(10);
+        int i = 0;
+
+        while (*(str + i) != '\0')
+        {
+                putchar(*(str + i));
+                i++;
+        }
+        putchar(10);
 }
