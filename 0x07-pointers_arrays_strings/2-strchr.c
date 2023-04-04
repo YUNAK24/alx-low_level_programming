faf6643b3063a4f76765b2178abbf1611dc7ec0a
yunak24 <yunakoyee@gmail.com>
2023-04-04 15:18:28 -0700

    strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio>
 
 /**
-*_strchr - locate a character in a string
-*@s: string to check
-*@c: character to check for
-*Return: pointer to spot in s with c or null
-*/
+ * _strchr - locate a character in a string
+ * @s: string to check
+ * @c: character to check for
+ * Return: pointer to spot in s with c or null
+ */
 
 char *_strchr(char *s, char c)
 {
