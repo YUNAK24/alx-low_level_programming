9ec442f6103121afb8d3c830462d7b90b2e3e982
yunak24 <yunakoyee@gmail.com>
2023-04-05 15:15:42 -0700

    modified 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-*_sqrt_recursion - returns the natural square root of a number
-*@n: number to be used
-*Return: the square root  of n
-*/
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be used
+ *
+ * Return: the square root of n
+ */
 
 int _sqrt_recursion(int n)
 {
@@ -14,11 +15,12 @@ int _sqrt_recursion(int n)
 }
 
 /**
-*_sqrt - returns the square root of a number
-*@n: test number
-*@x: squared number
-*Return: the square root of n
-*/
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
+ *
+ * Return: the square root of n
+ */
 int _sqrt(int n, int x)
 {
 	if (n > x / 2)
