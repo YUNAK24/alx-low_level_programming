c2608321c891df8ed9947ea5ccf3376096f0f67f
yunak24 <yunakoyee@gmail.com>
2023-04-05 10:11:37 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 /**
 *_sqrt_recursion - returns the natural square root of a number
 *@n: number to be used
-*Return: square root of n
+*Return: the square root  of n
 */
 
 int _sqrt_recursion(int n)
