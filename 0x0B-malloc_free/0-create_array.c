6d1347a8add7d9c66aba7d646e68c54f43ad2d0a
yunak24 <yunakoyee@gmail.com>
2023-04-11 16:24:36 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,9 +7,10 @@
 * @c: char to assign
 * Description: create array of size and assign char c
 * Return: pointer to array, NULL if fail
+*
 */
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c);
 {
 	char *str;
 	unsigned int i;
